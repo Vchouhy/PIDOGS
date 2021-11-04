eb9ebd557dd8c9d2f3ac9656bfe44a6ce8b5c68c
Vchouhy <81658991+Vchouhy@users.noreply.github.com>
2021-11-04 11:29:02 -0300

    Delete client directory

--- client/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
-
-
-
-export const Store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
